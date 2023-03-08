9c11ffc358901d4eb964fcbf903656d2a6b730d7
DanielRocha <rochadaniel.dev@gmail.com>
2023-03-08 20:21:38 -0300

    Ritira programação do projetil

--- culturaDeEstanciaTCC/Assets/script/bala.cs
+++ culturaDeEstanciaTCC/Assets/script/bala.cs
@@ -4,25 +4,6 @@ using UnityEngine;
 
 public class atirar : MonoBehaviour
 {
-    public int dano;
-    public float tempoDevida;
-    public float distancia;
-    public LayerMask layerInimig;
-   
 
-    void start()
-    {
-        Invoke("DestruirProjetil", tempoDevida);
-    }
-
-    private void Update()
-    {
-        
-
-    }
     
-    void DestruirProjetil()
-    {
-        Destroy(gameObject);
-    }
 }
